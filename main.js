5aaa2ee65dd422d7a1690f7c47a158d489665346
maxi <onwujekwechibuzor@gmail.com>
2023-09-10 03:14:18 +0100

    first commit

--- main.js
+++ main.js
@@ -4,20 +4,14 @@ document.addEventListener('DOMContentLoaded',()=>{
    updateDaysOfTheWeek = () =>{  
      date = new Date()
     const currentDay = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-    const day = date.getDay()
+    const day = date.getUTCDay()
     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent +=  currentDay[day]
     console.log(day)
 }
    updateDate = () =>{
      date = new Date()
-    let seconds = date.getSeconds()
-    let minutes = date.getMinutes()
-    let hours = date.getHours()
-    seconds = seconds < 10 ?seconds = '0'+seconds:seconds
-    minutes =minutes < 10 ?minutes = '0'+minutes:minutes
-    hours = hours < 10 ? hours = '0'+hours:hours
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = `Current UTC Time:  ${hours}: ${minutes}: ${seconds}`
-   
+     let time = date.getTime()
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = `${time}`
 }
 updateDaysOfTheWeek()
 updateDate()
